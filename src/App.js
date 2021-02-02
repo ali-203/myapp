32f219d299c69549f6c968b7bc2680666234dd20
ali-203 <62180010+ali-203@users.noreply.github.com>
2021-02-03 00:58:59 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import "./App.css";
+
+
+ 
+
+function App() {
+    return (
+    <>
+        <div className="main"> 
+        
+            <div className="bitron_main">
+                <img className="logo_img" src="logo.png" />
+
+                <div className="decentrlized">
+                    <h2 className="best">BEST DECENTRALIZED</h2>
+                    <h2 className="community">COMMUNITY SUPPORT FUND ON TRON BLOCKCHAIN</h2>
+                    <h2 className="verified">100% VERIFIED SMART CONTRACT</h2>
+                    <h3 className="daily">18.28% Daily ROI128% IN 7 DAYS</h3>
+                </div>
+                <div className="button_div">
+                    <button className="tele">TELEGRAM</button>
+                    <button className="smart_contract">SMART CONTRACT</button>
+                </div>
+                
+            </div>
+            <div className="image_money">
+            <img src="money.png" />
+        
+            </div>
+            
+        </div>
+        
+        
+        
+        
+        <div className="main2">
+        <div className="global">
+        <h1>Global Stats</h1>
+        </div>
+        <div className="three_div">
+        <h1>0</h1>
+        <h2>Total Invested TRX</h2>
+        <div className="total_trx">
+        <h1>0</h1>
+        <h2>Total Invested TRX</h2>
+            <h1>0</h1>
+            <h2>Total Investors</h2>
+        </div>
+        <div className="total_invested">
+            <h1>0</h1>
+            <h2>Total Dividends</h2>
+        </div>
+        <div className="total_dividen"></div>
+        
+        </div>
+
+        </div>
+        
+        </>
+        
+    )
+}
+
+export default App;
